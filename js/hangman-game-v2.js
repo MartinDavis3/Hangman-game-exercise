3377b41f61fb7079cff671a1e5f80b2280b8e02c
Martin Davis <martin_davis3@yahoo.ca>
2020-03-10 21:39:58 -0600

    add code for output

--- js/hangman-game-v2.js
+++ js/hangman-game-v2.js
@@ -2,31 +2,46 @@ var gameState = {
     secretWord = '',
     secretLetters: [],
     guessedState: [],
-    wrongGuesses:[],
+    guessedWord: '',
+    wrongGuesses:'',
+    leftToGuess: 0,
     triesLeft: 6,
     newWord: function ( word ) {
         for ( var i; i < word.length; i++) {
             this.secretLetters[i] = word.slice( i, i + 1 );
             this.guessedState[i] = ['_'];
+            this.guessedWord = this.guessedWord + '_';
         }
-        this.wrongGuesses = [];
+        this.wrongGuesses = '';
+        this.leftToGuess = word.length;
         this.triesLeft = 6;
     },
-    letterCorrect: function ( c ) {
+    letterCorrect: function ( testChar ) {
         var letterFound = false;
+        guessedWord = '';
         for (var i; i < this.secretLetters.length; i++) {
-            if ( c === secretLetters[i] ) {
-                guessedState[i] = secretLetters[i];
+            if ( testChar === this.secretLetters[i] ) {
+                this.guessedState[i] = this.secretLetters[i];
+                this.leftToGuess--;
                 letterFound = true;
             }
+            this.guessedWord = this.guessedWord + this.guessedState[i]
+        }
+        if ( !letterFound ) {
+            this.triesLeft--;
+            this.wrongGuesses = this.wrongGuesses + testChar;
         }
         return letterFound;
     },
-    wordCorrect: function ( w ) {
+    wordCorrect: function ( testWord ) {
         var wordFound = false;
-        if ( w === secretWord ) {
-            guessedState = secretLetters;
+        if ( testWord === this.secretWord ) {
+            this.guessedState = this.secretLetters;
+            this.guessedWord = this.secretWord;
+            this.leftToGuessed = 0;
             wordFound = true;    
+        } else {
+            this.triesLeft--;
         }
         return wordFound;
     }
@@ -40,23 +55,42 @@ guessForm.addEventListener( 'submit', function ( event ) {
 
     // Get the input
     var guessField = document.getElementById( 'guess-input' );
-
-    // Extract the value
     var guessValue = guessField.value;
     guessValue = guessValue.toLowerCase();
 
-    var result= false
+    var result = false;
+    var wordTested = false;
 
     if (guessValue.length > 1) {
-        result = gameState.wordCorrect( guessValue )
+        wordTested = true;
+        result = gameState.wordCorrect( guessValue );
     } else {
-        result = gameState.letterCorrect( guessValue )
+        result = gameState.letterCorrect( guessValue );
     }
-
+    outputState()
+    var communicationPara = document.getElementById( 'communication');
+    if ( gameState.leftToGuess === 0 ) {
+        communicationPara.textContent = "Congratulations! You win!";
+    } else if ( gameState.triesLeft === 0 ) {
+        communicationPara.textContent = "Bad luck. Out of tries. You lose!";
+    } else if ( result ) {
+        communicationPara.textContent = "Good guess!";
+    } else {
+        communicationPara.textContent = "Nope! hard luck.";
+    }
 } ) ;
 
 function startNewGame() {
-    gameState.newWord( 'developer' )
+    gameState.newWord( 'shopping' );
+}
+
+function outputState() {
+    var GuessPara = document.getElementById( 'guess-state' );
+    GuessPara.textContent = gameState.guessedWord;
+    var uncontainedSpan = document.getElementById( 'uncontained-letters' );
+    uncontainedSpan.textContent = gameState.wrongGuesses;
+    var triesLeftSpan = document.getElementById( 'tries-left' );
+    triesLeftSpan.textContent = gameState.triesLeft;
 }
 
 
